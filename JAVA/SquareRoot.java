2f3cca862bf3582a5f15505a816b18a29d0fd7e6
Shalu Sharma <59021479+shalu103@users.noreply.github.com>
2020-07-26 07:53:42 +0530

    Create SquareRoot.java

---
+++ JAVA/SquareRoot.java
@@ -0,0 +1,17 @@
+import java.lang.Math;
+import java.util.*;
+class Example{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		double n=sc.nextInt();
+		int sq,sqr;
+		if(n<0){
+			return;
+		}
+		
+		else
+			System.out.println(Math.sqrt(n)); // it is n by inbult function
+			 
+		
+	}
+}
