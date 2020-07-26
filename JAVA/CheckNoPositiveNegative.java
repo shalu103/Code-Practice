e6eae2ecd62ea98b85daee5ae6383069622d021c
Shalu Sharma <59021479+shalu103@users.noreply.github.com>
2020-07-26 06:33:21 +0530

    Create CheckNoPositiveNegative.java

---
+++ JAVA/CheckNoPositiveNegative.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Example{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		int n=sc.nextInt();
+		if(n>0){
+			System.out.println("Given no is positive");
+		}
+		
+		else{
+			System.out.println("Given no is negative");
+		}
+	}
+}
