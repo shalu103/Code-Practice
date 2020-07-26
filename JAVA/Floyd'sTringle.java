655c4404d900d874fdbff0edf529cdf112469491
Shalu Sharma <59021479+shalu103@users.noreply.github.com>
2020-07-26 06:57:55 +0530

    Create Floyd'sTringle.java

---
+++ JAVA/Floyd'sTringle.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Floy'dTriangle{
+	public static void main(String []args){
+		int number;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the no. of rows");
+		int n = sc.nextInt();
+		for(int i=0;i<n;i++){
+			for(int j=0;j<=i;j++){
+				System.out.println(number+" ");
+				number++;
+			}
+			System.out.println();
+				
+			
+		}
+	}
+}
